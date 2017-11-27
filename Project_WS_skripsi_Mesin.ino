b9761b62092bc7e62fe76534d2aa027cfee08312
william chandra <williamchans57@gmail.com>
2017-11-27 21:50:32 +0700

    Add files via upload

--- Project_WS_skripsi_Mesin.ino
+++ Project_WS_skripsi_Mesin.ino
@@ -1,4 +1,4 @@
-#include <UTFT.h> 
+#include <UTFT.h>
 #include <URTouch.h>
 #include <UTFT_Buttons.h>
 #include <AccelStepper.h>
@@ -15,7 +15,7 @@ extern uint8_t BigFont[];
 int x, y;
 //calibrator
 float calibration_factor = -96650;
-//==== Define variable 
+//==== Define variable
 int encoder_pin = 18; // pulse output from the module
 volatile byte pulses = 0; // number of pulses
 unsigned long timeold = 0;
@@ -31,29 +31,29 @@ int condition = 0;
 int propeller = 1;
 int motorspeed = 0;
 //bufferprintLCD
-char bufferS70[32]= "";
-char value[10]="";
+char bufferS70[32] = "";
+char value[10] = "";
 // Setup oneWire Dallas Temperature
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DeviceAddress insideThermometer, outsideThermometer;
-HX711 scale(DOUT,CLK);
+HX711 scale(DOUT, CLK);
 //==== Creating Objects of the LCD touch
-UTFT myGLCD(SSD1289,38,39,40,41); //Parameters should be adjusted to your Display/Shield model
+UTFT myGLCD(SSD1289, 38, 39, 40, 41); //Parameters should be adjusted to your Display/Shield model
 URTouch myTouch( 6, 5, 4, 3, 2);
 UTFT_Buttons  myButtons(&myGLCD, &myTouch);
-// Define a stepper motor 1 for arduino 
+// Define a stepper motor 1 for arduino
 // direction Digital 10 (CW), pulses Digital 11 (CLK)
-AccelStepper myStepper(1, 10,11);
+AccelStepper myStepper(1, 10, 11);
 void setup() {
-// Initial setup
+  // Initial setup
   Serial.begin(9600);
   LCDSetup();
   senseSetup();
 }
 // ====== Main setup ======
 //setup our LCD
-void LCDSetup(){
+void LCDSetup() {
   myGLCD.InitLCD();
   myGLCD.clrScr();
   myTouch.InitTouch();
@@ -61,7 +61,7 @@ void LCDSetup(){
   drawHomeScreen();  // Draws the Home Screen
   drawtime();
 }
-void senseSetup(){
+void senseSetup() {
   pinMode(encoder_pin, INPUT);
   //Interrupt 5 is digital pin 18
   //Triggers on Falling Edge (change from HIGH to LOW)
@@ -74,14 +74,14 @@ void senseSetup(){
 }
 void counter()
 {
-   //Update count
-   pulses++;
+  //Update count
+  pulses++;
 }
-void loop() { 
-  if(condition == 1){
+void loop() {
+  if (condition == 1) {
     speedsense();
     tempload();
-    motorservice(); 
+    motorservice();
     viscosityeq();
   }
   clicktft();
@@ -89,41 +89,41 @@ void loop() {
 }
 // ====== Custom Funtions ======
 
-void speedsense(){
-   if (millis() - timeold >= 1000) {
-      //Don't process interrupts during calculations
-      detachInterrupt(5);
-      velocityactual = (60 * 1000 / pulsesperturn )/ (millis() - timeold)* pulses;
-      timeold = millis();
-      pulses = 0;
-      //Restart the interrupt processing
-      attachInterrupt(5, counter, FALLING);
-   }
+void speedsense() {
+  if (millis() - timeold >= 1000) {
+    //Don't process interrupts during calculations
+    detachInterrupt(5);
+    velocityactual = (60 * 1000 / pulsesperturn ) / (millis() - timeold) * pulses;
+    timeold = millis();
+    pulses = 0;
+    //Restart the interrupt processing
+    attachInterrupt(5, counter, FALLING);
+  }
 }
 
-void viscosityeq(){
-  switch (propeller){
+void viscosityeq() {
+  switch (propeller) {
     case 1:
-      
+
       break;
     case 2:
-      
+
       break;
     case 3:
-      
+
       break;
     case 4:
-      
+
       break;
     default:
       break;
   }
 }
-void motorservice(){
+void motorservice() {
   if (velocity > 0) {
-    if ((velocityactual>velocity)&&(motorspeed>velocity-30)){
+    if ((velocityactual > velocity) && (motorspeed > velocity - 30)) {
       motorspeed--;
-    }else if((velocityactual<velocity) &&(motorspeed<velocity+30)){
+    } else if ((velocityactual < velocity) && (motorspeed < velocity + 30)) {
       motorspeed++;
     }
     myStepper.setSpeed(motorspeed);
@@ -132,19 +132,19 @@ void motorservice(){
   }
 }
 // get temperature information
-void tempload(){
+void tempload() {
   sensors.requestTemperatures(); // Send the command to get temperatures
   temperature = sensors.getTempCByIndex(0);
-  force = 9.78*scale.get_units();
+  force = 9.78 * scale.get_units();
 }
 // drawing our home screen
 void drawHomeScreen() {
   myGLCD.fillScr(VGA_WHITE);
   myGLCD.setColor(VGA_YELLOW); // Sets color to green
-  myGLCD.drawLine(0,160,319,160); // Draws the green line
-  myGLCD.drawLine(0,195,319,195); // Draws the green line
-  myGLCD.drawLine(107,160,107,239);
-  myGLCD.drawLine(213,160,213,239);
+  myGLCD.drawLine(0, 160, 319, 160); // Draws the green line
+  myGLCD.drawLine(0, 195, 319, 195); // Draws the green line
+  myGLCD.drawLine(107, 160, 107, 239);
+  myGLCD.drawLine(213, 160, 213, 239);
   myGLCD.fillRoundRect (4, 200, 103, 235);
   myGLCD.fillRoundRect (112, 200, 158, 235);
   myGLCD.fillRoundRect (162, 200, 208, 235);
@@ -160,12 +160,12 @@ void drawHomeScreen() {
   myGLCD.setColor(VGA_BLUE); // Sets color to redorange
   myGLCD.setFont(SmallFont); // Sets font to small
   myGLCD.print("Set Condition", 3, 170); // Prints the string on the screen
-  myGLCD.print("Set Speed",125, 170); // Prints the string on the screen
-  dtostrf(velocity,8,2,value);
-  sprintf(bufferS70,"Velocity = %s rpm",value);
-  myGLCD.print(bufferS70, 4,125 ); // Prints the string on the screen
+  myGLCD.print("Set Speed", 125, 170); // Prints the string on the screen
+  dtostrf(velocity, 8, 2, value);
+  sprintf(bufferS70, "Velocity = %s rpm", value);
+  myGLCD.print(bufferS70, 4, 125 ); // Prints the string on the screen
   myGLCD.print("Set Propeller", 215, 170); // Prints the string on the screen
-  sprintf(bufferS70,"Propeller %d",propeller);
+  sprintf(bufferS70, "Propeller %d", propeller);
   myGLCD.print(bufferS70, RIGHT, 125); // Prints the string on the screen
   myGLCD.setBackColor(VGA_YELLOW);
   myGLCD.setFont(BigFont);
@@ -176,87 +176,87 @@ void drawHomeScreen() {
   myGLCD.print(">", 284, 210);
 }
 //draw time
-void drawtime(){
+void drawtime() {
   // Title
   myGLCD.setBackColor(VGA_WHITE);
   myGLCD.setColor(VGA_BLUE); // Sets color to redorange
   myGLCD.setFont(SmallFont); // Sets font to small
-  dtostrf(temperature,8,2,value);
-  sprintf(bufferS70,"Temperature = %s C",value);
+  dtostrf(temperature, 8, 2, value);
+  sprintf(bufferS70, "Temperature = %s C", value);
   myGLCD.print(bufferS70, 4, 5); // Prints the string on the screen
-  dtostrf(force,8,2,value);
-  sprintf(bufferS70,"Force = %s N",value);
+  dtostrf(force, 8, 2, value);
+  sprintf(bufferS70, "Force = %s N", value);
   myGLCD.print(bufferS70, 4, 35); // Prints the string on the screen
-  dtostrf(viscosity,8,2,value);
-  sprintf(bufferS70,"Viscosity = %s Pa.s",value);
+  dtostrf(viscosity, 8, 2, value);
+  sprintf(bufferS70, "Viscosity = %s Pa.s", value);
   myGLCD.print(bufferS70, 4, 65); // Prints the string on the screen
-  dtostrf(velocityactual,8,2,value);
-  sprintf(bufferS70,"Velocity Actual = %s rpm",value);
+  dtostrf(velocityactual, 8, 2, value);
+  sprintf(bufferS70, "Velocity Actual = %s rpm", value);
   myGLCD.print(bufferS70, 4, 95); // Prints the string on the screen
 }
 
 // function algorithm check
-void clicktft(){
-  if (myTouch.dataAvailable()){
-      myTouch.read();
-      x = myTouch.getX();
-      y = myTouch.getY();
-    // If we press the UpSpeed  Button 
-    if ((x>=162) && (x<=208) && (y>=200) && (y<=235)) {
-      if (velocity<=580){
-        velocity+=20;
+void clicktft() {
+  if (myTouch.dataAvailable()) {
+    myTouch.read();
+    x = myTouch.getX();
+    y = myTouch.getY();
+    // If we press the UpSpeed  Button
+    if ((x >= 162) && (x <= 208) && (y >= 200) && (y <= 235)) {
+      if (velocity <= 580) {
+        velocity += 20;
         myGLCD.setBackColor(VGA_WHITE);
         myGLCD.setColor(VGA_BLUE); // Sets color to redorange
         myGLCD.setFont(SmallFont); // Sets font to small
-        dtostrf(velocity,8,2,value);
-        sprintf(bufferS70,"Velocity = %s rpm",value);
-        myGLCD.print(bufferS70, 4,125 ); // Prints the string on the screen
+        dtostrf(velocity, 8, 2, value);
+        sprintf(bufferS70, "Velocity = %s rpm", value);
+        myGLCD.print(bufferS70, 4, 125 ); // Prints the string on the screen
       }
     }
-    // If we press the DownSpeed Button 
-    if ((x>=112) && (x<=158) && (y>=200) && (y<=235)) {
-      if (velocity>=20){
-        velocity-=20;
+    // If we press the DownSpeed Button
+    if ((x >= 112) && (x <= 158) && (y >= 200) && (y <= 235)) {
+      if (velocity >= 20) {
+        velocity -= 20;
         myGLCD.setBackColor(VGA_WHITE);
         myGLCD.setColor(VGA_BLUE); // Sets color to redorange
         myGLCD.setFont(SmallFont); // Sets font to small
-        dtostrf(velocity,8,2,value);
-        sprintf(bufferS70,"Velocity = %s rpm",value);
-        myGLCD.print(bufferS70, 4,125 ); // Prints the string on the screen
+        dtostrf(velocity, 8, 2, value);
+        sprintf(bufferS70, "Velocity = %s rpm", value);
+        myGLCD.print(bufferS70, 4, 125 ); // Prints the string on the screen
       }
     }
-    // If we press the UpProppeler Button 
-    if ((x>=268) && (x<=314) && (y>=200) && (y<=235)) { 
-      if (propeller<4){
+    // If we press the UpProppeler Button
+    if ((x >= 268) && (x <= 314) && (y >= 200) && (y <= 235)) {
+      if (propeller < 4) {
         propeller++;
         myGLCD.setBackColor(VGA_WHITE);
         myGLCD.setColor(VGA_BLUE); // Sets color to redorange
         myGLCD.setFont(SmallFont); // Sets font to small
-        sprintf(bufferS70,"Propeller %d",propeller);
+        sprintf(bufferS70, "Propeller %d", propeller);
         myGLCD.print(bufferS70, RIGHT, 125); // Prints the string on the screen
-      } 
+      }
     }
-    // If we press the DownProppeler Button 
-    if ((x>=218) && (x<=264) && (y>=200) && (y<=235)) {
-      if (propeller>1){
+    // If we press the DownProppeler Button
+    if ((x >= 218) && (x <= 264) && (y >= 200) && (y <= 235)) {
+      if (propeller > 1) {
         propeller--;
         myGLCD.setBackColor(VGA_WHITE);
         myGLCD.setColor(VGA_BLUE); // Sets color to redorange
         myGLCD.setFont(SmallFont); // Sets font to small
-        sprintf(bufferS70,"Propeller %d",propeller);
+        sprintf(bufferS70, "Propeller %d", propeller);
         myGLCD.print(bufferS70, RIGHT, 125); // Prints the string on the screen
       }
     }
     // Condition
-    if ((x>=4) && (x<=103) && (y>=200) && (y<=235)) {
+    if ((x >= 4) && (x <= 103) && (y >= 200) && (y <= 235)) {
       myGLCD.setColor(VGA_BLUE); // Sets color to redorange
       myGLCD.setBackColor(VGA_YELLOW);
       myGLCD.setFont(BigFont);
-      if (condition == 0){
+      if (condition == 0) {
         condition = 1;
         motorspeed = (int) velocity;
         myGLCD.print(" ON ", 20, 210);
-      }else{
+      } else {
         motorspeed = velocity;
         velocityactual = 0.0;
         temperature = 0.0;
